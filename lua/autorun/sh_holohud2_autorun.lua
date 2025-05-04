2ebf7db2cad57a7c8f0479e82b6e1044391204f8
DyaMetR <flaberto.abc@gmail.com>
2025-05-04 19:06:07 +0200

    Updated timestamp

--- lua/autorun/sh_holohud2_autorun.lua
+++ lua/autorun/sh_holohud2_autorun.lua
@@ -14,7 +14,7 @@ HOLOHUD2 = HOLOHUD2 or {}
 HOLOHUD2.Name           = "D/GL4 HUD"
 HOLOHUD2.CodeName       = "D/GL4" -- overly technical nomenclature for aesthetic purposes
 HOLOHUD2.Version        = "1.7.1"
-HOLOHUD2.Date           = 1745333990 -- epoch timestamp of the build date
+HOLOHUD2.Date           = 1746378173 -- epoch timestamp of the build date
 HOLOHUD2.Credits        = { -- { name, { contribution, ... } }
 
     { "DyaMetR", { "code", "design", "art", "locale_es-ES" } },
