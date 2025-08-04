ab488b345fd99e8978e1406f95b5beb8bdc0d485
TheOnly8Z <888888zombies@gmail.com>
2025-08-04 19:09:20 -0400

    Remove print statement from VJ addon support

--- lua/holohud2/add-ons/vjbase.lua
+++ lua/holohud2/add-ons/vjbase.lua
@@ -6,13 +6,13 @@
 if not VJ then return end
 
 if SERVER then
-    
+
     hook.Add( "OnEntityCreated", "holohud2_vjbase", function( ent )
-    
+
         if not ent.IsVJBaseSNPC then return end
 
         for _, class in pairs( ent.VJ_NPC_Class ) do
-            print(class)
+
             if class ~= "CLASS_PLAYER_ALLY" then continue end
 
             ent:SetNW2Bool( "holohud2_vjbase_ally", true )
